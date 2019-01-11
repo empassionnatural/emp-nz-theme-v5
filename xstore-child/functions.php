a1debfb1d6d26fb64cbb15d93a057a3a95b078bb
Joneel <web@empassion.com.au>
2019-01-11 14:02:20 +0800

    #revert fontawesome icon

--- xstore-child/functions.php
+++ xstore-child/functions.php
@@ -39,7 +39,7 @@ function empdev_custom_scripts_frontend(){
         wp_enqueue_style('font-lobster-css-style', 'https://fonts.googleapis.com/css?family=Lobster+Two', array(), '1.1.10');
         wp_enqueue_style('home-custom-style', get_stylesheet_directory_uri() . '/css/home-custom-style.css' , array(), '1.1.10');
     }
-    wp_enqueue_style( 'fontawesome-css-style', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css' , array(), '1.1.9' );
+    //wp_enqueue_style( 'fontawesome-css-style', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css' , array(), '1.1.9' );
 	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/css/custom-style.css', array(), '3.1.4' );
     wp_enqueue_style( 'product-custom-style', get_stylesheet_directory_uri() . '/css/product-view.css' , array(), '1.1.9' );
 	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/custom-script.js', array('jquery'), '1.6.3', false );
