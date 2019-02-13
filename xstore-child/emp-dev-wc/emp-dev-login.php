30b0c5364a53c2ed2ad8320b9550d84f95166dff
wenceWeb-desu <web1@empassion.com.au>
2019-02-13 13:27:36 +0800

    css updated

--- xstore-child/emp-dev-wc/emp-dev-login.php
+++ xstore-child/emp-dev-wc/emp-dev-login.php
@@ -131,10 +131,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/ranges/signature-range/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_signature.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_signature.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_signature.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_signature.png">
                 <p>Signature Range</p>
               </div>';
     $html .= '</a>';
@@ -142,10 +142,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/ranges/lavender-range/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_lavender.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_lavender.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_lavender.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_lavender.png">
                 <p>Lavander</p>
               </div>';
     $html .= '</a>';
@@ -153,10 +153,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/aromatherapy/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_aromatherapy.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_aromatherapy.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_aroma.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_aroma.png">
                 <p>Aromatherapy</p>
               </div>';
     $html .= '</a>';
@@ -164,10 +164,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/men/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_men.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_men.jpg" />';
     $html .= "<div class='overlay'><span></span></div>";
     $html .= "<div class='bg-color-txt-pic'>
-                <img src='/wp-content/uploads/2018/10/icon_section_men.png'>
+                <img src='/wp-content/uploads/2019/02/icon_section_men.png'>
                 <p>Men's Range</p>
               </div>";
     $html .= '</a>';
@@ -175,10 +175,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/age-ranges/teen-range/teen-boys/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_teen_boys.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_teen_boys.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_teenboys.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_teenboys.png">
                 <p>Teen Boys</p>
               </div>';
     $html .= '</a>';
@@ -186,10 +186,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/age-ranges/teen-range/teen-girls/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_teen_girls.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_teen_girls.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_teengirls.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_teengirls.png">
                 <p>Teen Girls</p>
               </div>';
     $html .= '</a>';
@@ -197,10 +197,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/age-ranges/kids/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_kids.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_kids.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_kids.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_kids.png">
                 <p>Kids</p>
               </div>';
     $html .= '</a>';
@@ -208,10 +208,10 @@ function product_form($atts) {
 
     $html .= '<div class="box">';
     $html .= '<a href="product-category/age-ranges/baby/">';
-    $html .= '<img src="/wp-content/uploads/2018/10/thumb_baby.jpg" />';
+    $html .= '<img src="/wp-content/uploads/2019/02/thumb_baby.jpg" />';
     $html .= '<div class="overlay"><span></span></div>';
     $html .= '<div class="bg-color-txt-pic">
-                <img src="/wp-content/uploads/2018/10/icon_section_baby.png">
+                <img src="/wp-content/uploads/2019/02/icon_section_baby.png">
                 <p>Baby</p>
               </div>';
     $html .= '</a>';
