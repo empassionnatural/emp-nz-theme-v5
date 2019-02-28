8ad26cba27d5a348f81f0b246a5f025890df63d2
wenceWeb-desu <web1@empassion.com.au>
2019-02-28 11:34:15 +0800

    updates css, js & functions

--- xstore-child/js/custom-script.js
+++ xstore-child/js/custom-script.js
@@ -124,8 +124,6 @@ var openAccordion = function(evt , id1 , id2) {
 var $win = $(window);
 
 $win.on('scroll', function () {
-    console.log($win.scrollTop());
-
     if ($win.scrollTop() < 1) {
         $('#id-floatingbutton').addClass('fbOut').removeClass('fbIn');
     }
